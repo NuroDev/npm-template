e6e0e24f30868d708d50a0bf472ab49b7e75de42
nuro <4991309+NuroDev@users.noreply.github.com>
2022-07-02 02:06:51 +0000

    Updated Vitest ~ import alias directory name

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ import { resolve } from "node:path";
 export default defineConfig({
   resolve: {
     alias: {
-      "~": resolve(__dirname, "/src"),
+      "~": resolve(__dirname, "src"),
     },
   },
   test: {
